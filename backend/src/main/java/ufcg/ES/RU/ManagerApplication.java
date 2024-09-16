781965471ef7bc671c55adaa19ddc79e0cf9b759
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2024-09-15 22:21:34 -0300

    no message

--- backend/src/main/java/ufcg/ES/RU/ManagerApplication.java
+++ backend/src/main/java/ufcg/ES/RU/ManagerApplication.java
@@ -7,7 +7,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "TCC Manager", version = "1", description = "API desenvolvida para a cadeira Projeto de Software para facilitar a definição de temas e orientações de TCC a partir de interesses comuns entre docentes e discentes"))
+@OpenAPIDefinition(info = @Info(title = "RU Manager", version = "1", description = "API desenvolvida para a cadeira "
+
+))
 public class ManagerApplication {
 
 	public static void main(String[] args) {
