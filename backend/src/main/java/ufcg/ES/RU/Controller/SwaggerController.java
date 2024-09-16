362d0004f6cbe4b771ef1e364fc33b9096548d35
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2024-09-16 12:42:04 -0300

    redirecionar para o swagger

--- backend/src/main/java/ufcg/ES/RU/Controller/SwaggerController.java
+++ backend/src/main/java/ufcg/ES/RU/Controller/SwaggerController.java
@@ -1,2 +1,12 @@
-package ufcg.ES.RU.Controller;public class SwaggerController {
+package ufcg.ES.RU.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerController {
+    @GetMapping("/")
+    public String redirectToSwaggerUi() {
+        return "redirect:/swagger-ui.html";
+    }
 }
