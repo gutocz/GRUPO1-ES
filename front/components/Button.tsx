595fcb8426aa8addd7755836f28a222b4e62946b
soutofernando <fernandosoutol45@gmail.com>
2024-09-16 18:09:44 -0300

    feat: home

--- front/components/Button.tsx
+++ front/components/Button.tsx
@@ -1,12 +1,46 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+type Attributes =
+  | JSX.HTMLAttributes<HTMLButtonElement>
+  | JSX.HTMLAttributes<HTMLAnchorElement>;
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+function isAnchorProps(
+  props: Attributes,
+): props is JSX.HTMLAttributes<HTMLAnchorElement> {
+  return "href" in props;
+}
+
+interface Props {
+  primary?: boolean;
+  secondary?: boolean;
+}
+
+export function Button({
+  primary,
+  ...props
+}: Props & Attributes) {
+  const className = primary
+    ? "bg-ru-orange-500 rounded-full cursor-pointer leading-[18px] font-bold text-lg px-6 py-[18px] items-center"
+    : "text-ru-orange-500 rounded-full leading-[18px] cursor-pointer font-medium text-sm px-6 py-[18px] text-center";
+
+  if (isAnchorProps(props)) {
+    return (
+      <a
+        {...props}
+        class={className}
+      >
+        {props.children}
+      </a>
+    );
+  }
+
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-    />
+      class={className}
+    >
+      {props.children}
+    </button>
   );
 }
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -4,4 +4,15 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      ru: {
+        orange: {
+          "500": "#F77F00",
+        },
+      },
+    },
+  },
 } satisfies Config;
