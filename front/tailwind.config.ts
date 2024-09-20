71d52e379d7879d72ccd704ff407c78ee3ee00de
soutofernando <fernandosoutol45@gmail.com>
2024-09-20 15:27:56 -0300

    feat: add daisyui

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,18 +1,22 @@
 import { type Config } from "tailwindcss";
+import daisyui from 'daisyui';
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
   theme: {
-    colors: {
-      white: "#FFFFFF",
-      black: "#000000",
-      ru: {
-        orange: {
-          "500": "#F77F00",
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        ru: {
+          orange: {
+            "500": "#F77F00",
+          },
         },
       },
     },
   },
+  plugins: [daisyui],
 } satisfies Config;
